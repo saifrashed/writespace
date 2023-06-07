b77ecebb3ceec5bbc11dbc69e9157c5e721f8b71
Wessel <wessel.van.sommeren@gmail.com>
2023-06-07 13:08:17 +0200

    style fouten en autofix

--- backend-pse-e/src/app.js
+++ backend-pse-e/src/app.js
@@ -1,17 +1,17 @@
 const corsOptions = {
-    origin: "*",
-    credentials: true,
-    optionSuccessStatus: 200,
-    port: process.env.PORT,
-}
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+  port: process.env.PORT,
+};
 
-require("dotenv").config();
+require('dotenv').config();
 
 // Database connection
-require("./config/databaseConfig").connect();
+require('./config/databaseConfig').connect();
 
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 // Import ObjectId from MongoDB
 const { ObjectId } = require('mongodb');
@@ -19,11 +19,10 @@ const { ObjectId } = require('mongodb');
 const app = express();
 
 // Cross origin resource sharing
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 // Express
-app.use(express.json())
-app.use(express.json({ limit: "50mb" }));
-
+app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // ************************* Add new services here *************************
 // Import services here
@@ -31,22 +30,21 @@ const testService = require('./services/test.service.js');
 // Define new routes here with the start route
 app.use('/test', testService);
 
-
 // ************************* General requests *************************
-app.get("/", (req, res) => {
-    res.status(200).json('Welcome to PSE-E Api')
+app.get('/', (req, res) => {
+  res.status(200).json('Welcome to PSE-E Api');
 });
 
 // Every other route. This should be the last route else it won't work
-app.use("*", (req, res) => {
-    res.status(404).json({
-        success: "false",
-        message: "Page not found",
-        error: {
-            statusCode: 404,
-            message: "This route is not defined",
-        },
-    });
+app.use('*', (req, res) => {
+  res.status(404).json({
+    success: 'false',
+    message: 'Page not found',
+    error: {
+      statusCode: 404,
+      message: 'This route is not defined',
+    },
+  });
 });
 
 module.exports = app;
