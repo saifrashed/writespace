1a17cbe1184df1c8bf815018ea687aefb2797324
CollinHva <Collin.poetoehena@hva.nl>
2023-06-07 09:56:19 +0200

    Cleanup + added explanation

--- backend-pse-e/src/services/canvas-api.service.js
+++ backend-pse-e/src/services/canvas-api.service.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const { ObjectId } = require('mongodb');
 
-// Require axios for the canvas api
+// Require axios for communicating with the canvas api
 const axios = require('axios');
 
 // Canvas api url
 const apiUrl = 'https://canvas.uva.nl/api/v1';
 // Change this access token every time for different users!
-const accessToken = '10392~gyicaEXku20FnjyEZWajtl9lDEAop2sHvpiGZl5DmqeqzhJzj0OtDq4bOs8AgdNm';
+// See Canvas_API document in discord on how to get an access token
+const accessToken = 'YOUR_ACCESS_TOKEN_HERE';
 
-// Get request to get all courses with the api
+/* 
+Get request to get all courses with the api.
+router.x is used to specify the used route.
+axios is used to communicate with the canvas api and authenticate
+the user with the access token.
+Postman request URL: localhost:5000/canvas-api/courses
+*/
 router.get('/courses', (req, res) => {
     axios.get(`${apiUrl}/courses`, {
       headers: {
+        // Authorization using the access token
         Authorization: `Bearer ${accessToken}`
       }
-    })
-      .then(response => {
+    }).then(response => {
         res.json(response.data);
-      })
-      .catch(error => {
+    }).catch(error => {
+        console.error('Error from Canvas API:', error);
         res.status(500).json({ error: 'An error occurred.' });
-      });
+    });
 });
-  
+
 module.exports = router;
