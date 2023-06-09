29c54064fb3a93a9151ada50a83694fc47df6ec0
Devran <devran_ilhan@live.nl>
2023-06-09 23:01:13 +0200

    added navbar and styling fixes

--- frontend-pse-e/pages/courses/[courseId]/index.tsx
+++ frontend-pse-e/pages/courses/[courseId]/index.tsx
@@ -9,6 +9,7 @@ import { motion } from "framer-motion";
 import { useContext, useEffect } from 'react';
 import { Context } from '@/Context';
 import { Enrollment } from "@/lib/types";
+import NavBar from "@/components/NavBar";
 
 const CourseOverview = () => {
   const router = useRouter();
@@ -55,8 +56,10 @@ const CourseOverview = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex flex-col md:flex-row">
-        <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-8 md:w-1/5 md:h-[100vh] flex items-center content-center justify-center">
+      <NavBar />
+
+      <div className="flex flex-col md:flex-row pt-20">
+        <div className="bg-white lg:border lg:border-gray-200 rounded-lg lg:shadow lg:dark:bg-gray-800 lg:dark:border-gray-700 p-8 md:min-h-screen flex items-center content-center justify-center">
           <div className="flex flex-col items-center">
             <motion.h5
               initial={{ opacity: 0 }}
@@ -81,13 +84,14 @@ const CourseOverview = () => {
                 <th scope="col" className="px-6 py-4 whitespace-nowrap">
                   Due At
                 </th>
-                <th scope="col" className="px-6 py-4 whitespace-nowrap">
-                  Submission Status
-                </th>
-                {isTeacher &&
+                {isTeacher ? (
                   <th scope="col" className="px-6 py-4 whitespace-nowrap">
                   </th>
-                }
+                ) : (
+                  <th scope="col" className="px-6 py-4 whitespace-nowrap">
+                    Submission Status
+                  </th>
+                )}
               </tr>
             </thead>
             <tbody>
@@ -102,30 +106,8 @@ const CourseOverview = () => {
                     <td className="px-6 py-4 whitespace-nowrap">
                       {formatDate(assignment.due_at)}
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div
-                        className={`flex items-center ${assignment.has_submitted_submissions
-                          ? ("text-emerald-400")
-                          : new Date() > new Date(assignment.due_at)
-                            ? ("text-orange-500")
-                            : ("text-red-500")
-                          } font-bold`}
-                      >
-                        <div
-                          className={`h-2.5 w-2.5 rounded-full ${assignment.has_submitted_submissions
-                            ? ("bg-emerald-400")
-                            : new Date() > new Date(assignment.due_at)
-                              ? ("text-orange-500")
-                              : ("bg-red-500")
-                            } mr-2`}
-                        ></div>
-                        {assignment.has_submitted_submissions
-                          ? "Submitted"
-                          : "Not Submitted"}
-                      </div>
-                    </td>
-                    {isTeacher &&
-                      <td className="flex items-center px-3 py-2 space-x-3">
+                    {isTeacher ? (
+                      <td className="flex items-center justify-end px-3 py-2 space-x-3">
                         <button className="flex p-2.5 bg-yellow-500 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white">
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
@@ -138,9 +120,32 @@ const CourseOverview = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
                           </svg>
                         </button>
-                        {/* <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                      <a href="#" className="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a> */}
                       </td>
+                    )
+                      : (
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div
+                            className={`flex items-center ${assignment.has_submitted_submissions
+                              ? ("text-emerald-400")
+                              : new Date() > new Date(assignment.due_at)
+                                ? ("text-orange-500")
+                                : ("text-red-500")
+                              } font-bold`}
+                          >
+                            <div
+                              className={`h-2.5 w-2.5 rounded-full ${assignment.has_submitted_submissions
+                                ? ("bg-emerald-400")
+                                : new Date() > new Date(assignment.due_at)
+                                  ? ("text-orange-500")
+                                  : ("bg-red-500")
+                                } mr-2`}
+                            ></div>
+                            {assignment.has_submitted_submissions
+                              ? "Submitted"
+                              : "Not Submitted"}
+                          </div>
+                        </td>
+                      )
                     }
                   </tr>
                 )
