12c46480dbcb48ccaff9f0356f63a9b8d2df290a
CollinHva <Collin.poetoehena@hva.nl>
2023-06-09 16:44:44 +0200

    Notes from meeting with Gerrit

--- backend-pse-e/src/services/canvas-api.service.js
+++ backend-pse-e/src/services/canvas-api.service.js
@@ -143,4 +143,34 @@ router.get('/login', (req, res) => {
   res.redirect(authUrl);
 });
 
+// API: gebruiker logt in op jou tool, authoriseert dingen in canvas. Alles wat de gebruiker kan.
+// TODO: wij willen de API aparte website en met uva account login, dus de API.
+// API developer key wordt aangemaakt voor ons. Simpeler om te implementeren.
+// OIDC OAuth login. 
+/*
+Developer key wordt aangemaakt voor ons door Gerrit.
+Daarvoor heeft hij nodig 1 of meer redirect URLs, waar canvas terug naar redirect na een geslaagde login. 
+Canvas stuurt die gegevens dan naar ons toe, naar die endpoint, 
+of een accessToken of een code waarmee je die accesstoken kan opvragen, maar dit staat wel in de documentatie van de canvas api.
+
+Daarna zou het moeten werken, met die key (vmg cliendId en ClientSecret).
+
+
+Flow:
+1. GET https://https://uvadlo-dev.test.instructure.com/login/oauth2/auth?client_id=<clientIdFromGerrit>&response_type=code&state=<Number, such as 1>&redirect_uri=<What I Sent to Gerrit, can be any>
+2. In the response it sends a code and state, the code can be used to ask the access token of the user.
+  You get the access token and the refresh token, the access token expires after an hour. If you want a new one you can use the refresh token.
+3. Step 3 needs to happen server side because the secret is sent with it. grant_type is just the string "authorization_code"
+
+Test omgeving wordt elke maand leeggegooit, 1x per maand. Dan moet het opnieuw geconfigureerd worden.
+*/
+
+// TODO: mail sturen naar Gerrit voor de client id en client secret.
+
+/*
+LTI: de gebruiker zit in canvas en opent een assignment, die assignment stuurt de gebruiker door naar jou tool, waarbij er 
+gegevens worden meegestuurt. Je krijg hier de gegevens aangeleverd. Tools zoals CodeGrade.
+LTI is ook lastiger te implementeren, terwijl dat andere gewoon een login mechanisme is.
+*/
+
 module.exports = router;
