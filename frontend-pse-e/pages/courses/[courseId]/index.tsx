4adcb769bd1523dd44a2b6e5f88bf67235fb8b91
Devran <devran_ilhan@live.nl>
2023-06-12 01:18:43 +0200

    visual fix

--- frontend-pse-e/pages/courses/[courseId]/index.tsx
+++ frontend-pse-e/pages/courses/[courseId]/index.tsx
@@ -80,17 +80,18 @@ const CourseOverview = () => {
               className="mb-2 text-sm text-gray-500 dark:text-gray-400 text-center">
               {course?.course_code}
             </span>
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-              <div style={{ width: calculateSubmittedPercentage() }} className={`${course?.course_color || "bg-green-600"} opacity-60 saturate-150 h-2.5 rounded-full invert w-0 transition-[width] ease`}></div>
-            </div>
-            {isTeacher && !isLoading &&
-              <button type="button" className="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none font-medium rounded-2xl text-sm px-2.5 py-2.5 mt-5 text-center inline-flex items-center">
+            {isTeacher ? (
+              <button type="button" className="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none font-medium rounded-2xl text-sm px-2.5 py-2.5 text-center inline-flex items-center">
                 <svg className="h-5 w-5 me-1" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"></path>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"></path>
                 </svg>
                 New Assignment
               </button>
-            }
+            ) : (
+              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                <div style={{ width: calculateSubmittedPercentage() }} className={`${course?.course_color || "bg-green-600"} opacity-60 saturate-150 h-2.5 rounded-full invert w-0 transition-[width] ease`}></div>
+              </div>
+            )}
           </motion.div>
         </div>
         <div className="w-full relative overflow-x-auto shadow-md sm:p-2 md:p-4 lg:p-8 md:w-4/5">
