3e92bd92b2f289683dc48db49b7228b208bacedc
CollinHva <Collin.poetoehena@hva.nl>
2023-06-11 13:05:43 +0200

    Clean up

--- backend-pse-e/src/services/canvas-api.service.js
+++ backend-pse-e/src/services/canvas-api.service.js
@@ -134,27 +134,17 @@ Then here something with file upload!
 // TODO: Create assignment on canvas
 */
 
+// TODO: discuss with Saif and/or Devran how the FE can extract the code to do the requests for getting a user's token
 
-// TODO: use OAuth from Canvas documentation to extract access token?? Use Developer key settings??
-// Login request (not done yet, need developer key setup)
 // Route for initiating the login redirect
+// Test this by going to this URL in your browser for example: localhost:5000/canvas-api/login
 router.get('/login', (req, res) => {
   const authUrl = `${loginApiUrl}/login/oauth2/auth?client_id=${CLIENT_ID}&response_type=code&state=1&redirect_uri=${redirectUri}`;
   res.redirect(authUrl);
 });
-// TODO: to test this locally: go to this URL in your browser: localhost:5000/canvas-api/login
-/*
-TODO: after this (now you still get a tab with "Deze site kan geen beveiligde verbinding leveren", but that is because it is not handled
-in the FE, but you get the correct URL, with only "code" and "state", so there is no error, it is just because that URL is not handled
-in the FE!), you get the code and state in the frontend in the webbrowser, then the FE needs to send a request with the 
-received "code" and "state" to the backend to retrieve the user's access token!
-
-TODO: So, discuss with Saif and/or Devran how the FE can extract that from the URL to send the following request in the BE to 
-get the user access-key/token!
-*/
-// After the /login request, the FE extracted the code and state from the URL in the browser
+// After the /login request, the FE extracted the code from the URL in the browser
 // that can be used to make this request to actually get the user's access-key/token
-// NOTE: the code from the /login request can only be used for one request, otherwise it will give an error!!
+// NOTE: the code from the /login request can only be used for ONE request, otherwise it will give an error!!
 router.post('/get-user-token', (req, res) => {
   axios.post(`${loginApiUrl}/login/oauth2/token`, {}, {
     params: {
@@ -188,27 +178,5 @@ router.post('/get-user-token/refresh', (req, res) => {
     res.status(500).json({ error: 'An error occurred.' });
   });
 });
-
-// API: gebruiker logt in op jou tool, authoriseert dingen in canvas. Alles wat de gebruiker kan.
-// TODO: wij willen de API aparte website en met uva account login, dus de API.
-// API developer key wordt aangemaakt voor ons. Simpeler om te implementeren.
-// OIDC OAuth login. 
-/*
-Developer key wordt aangemaakt voor ons door Gerrit.
-Daarvoor heeft hij nodig 1 of meer redirect URLs, waar canvas terug naar redirect na een geslaagde login. 
-Canvas stuurt die gegevens dan naar ons toe, naar die endpoint, 
-of een accessToken of een code waarmee je die accesstoken kan opvragen, maar dit staat wel in de documentatie van de canvas api.
-
-Daarna zou het moeten werken, met die key (vmg cliendId en ClientSecret).
-
-
-Flow:
-1. GET https://uvadlo-dev.test.instructure.com/login/oauth2/auth?client_id=<clientIdFromGerrit>&response_type=code&state=<Number, such as 1>&redirect_uri=<What I Sent to Gerrit, can be any>
-2. In the response it sends a code and state, the code can be used to ask the access token of the user.
-  You get the access token and the refresh token, the access token expires after an hour. If you want a new one you can use the refresh token.
-3. Step 3 needs to happen server side because the secret is sent with it. grant_type is just the string "authorization_code"
-
-Test omgeving wordt elke maand leeggegooit, 1x per maand. Dan moet het opnieuw geconfigureerd worden.
-*/
 
 module.exports = router;
