0c5fdc0af7cd99e7488d15a4aa92b2c660f66cff
Devran <devran_ilhan@live.nl>
2023-06-12 00:11:33 +0200

    course icon change and textcolor to white

--- frontend-pse-e/pages/courses/index.tsx
+++ frontend-pse-e/pages/courses/index.tsx
@@ -37,7 +37,6 @@ const Courses = () => {
     "bg-yellow-500",
     "bg-amber-500",
     "bg-orange-500",
-    "bg-deepOrange-500",
     "bg-red-600",
     "bg-pink-600",
     "bg-purple-600",
@@ -86,22 +85,14 @@ const Courses = () => {
                       key={course?.id}
                       layoutId={course?.id.toString()}
                       className={`flex flex-col justify-center items-center ${cardColors[index]} bg-opacity-50 hover:cursor-pointer shadow-sm hover:shadow-lg rounded-2xl h-40 xs:h-64 p-2`}>
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-12 h-12 text-white">
-                        <title />
-                        <g id="Complete">
-                          <g id="browsers">
-                            <g>
-                              <rect fill="none" height="14" rx="2" ry="2" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" width="14" x="3" y="7" />
-                              <path d="M8,3H19a2,2,0,0,1,2,2V16" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
-                            </g>
-                          </g>
-                        </g>
+                      <svg className="w-12 h-12 text-white" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"></path>
                       </svg>
                       <h5
-                        className="text-black mt-2 font-medium text-center select-none">{course.name}
+                        className="text-white mt-2 font-medium text-center select-none">{course.name}
                       </h5>
                       <span
-                        className="mt-2 font-medium text-center select-none text-sm text-gray-500 dark:text-gray-400">{course.course_code}
+                        className="text-white opacity-60 mt-2 font-medium text-center select-none text-sm">{course.course_code}
                       </span>
                     </motion.div>
                   </Link>
