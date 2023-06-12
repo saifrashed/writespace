065948b305af7dcafdf5b7cd49c2120b3e0f90d3
Devran <devran_ilhan@live.nl>
2023-06-12 15:25:14 +0200

    hotfix cardcolor

--- frontend-pse-e/pages/courses/index.tsx
+++ frontend-pse-e/pages/courses/index.tsx
@@ -80,11 +80,11 @@ const Courses = () => {
             ) : (
               <>
                 {courses.map((course: Course, index: number) => (
-                  <Link href={"/courses/" + course.id} key={course.id} onClick={() => handleClick(course, cardColors[index])}>
+                  <Link href={"/courses/" + course.id} key={course.id} onClick={() => handleClick(course, cardColors[index % cardColors.length])}>
                     <motion.div
                       key={course?.id}
                       layoutId={course?.id.toString()}
-                      className={`flex flex-col justify-center items-center ${cardColors[index]} bg-opacity-50 hover:cursor-pointer shadow-sm hover:shadow-lg rounded-2xl h-40 xs:h-64 p-2`}>
+                      className={`flex flex-col justify-center items-center ${cardColors[index % cardColors.length]} bg-opacity-50 hover:cursor-pointer shadow-sm hover:shadow-lg rounded-2xl h-40 xs:h-64 p-2`}>
                       <svg className="w-12 h-12 text-white" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"></path>
                       </svg>
