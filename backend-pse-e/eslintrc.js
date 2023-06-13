edb8959a9c8a2ee9e4a1b51342af69d76a4c1420
Wessel <wessel.van.sommeren@gmail.com>
2023-06-13 12:05:09 +0200

    Added ci to backend

---
+++ backend-pse-e/eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+      jest: true, // Added for testing
+    },
+    extends: 'airbnb',
+    parserOptions: {
+      ecmaVersion: 'latest',
+    },
+    rules: {
+    },
+  };
