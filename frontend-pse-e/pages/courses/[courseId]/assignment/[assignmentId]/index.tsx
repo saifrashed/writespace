97a740a4f7eb1fa3f08105e585b527295bfd51cf
Ishana <ishana.bohorey@student.uva.nl>
2023-06-14 13:12:42 +0200

    error fix missing variable

--- frontend-pse-e/pages/courses/[courseId]/assignment/[assignmentId]/index.tsx
+++ frontend-pse-e/pages/courses/[courseId]/assignment/[assignmentId]/index.tsx
@@ -232,7 +232,7 @@ const Assignments = () => {
         <tbody>
           {submissions.map((obj) => (
             <tr
-              key={obj.id}
+              key={obj.anonymous_id}
               className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
             >
               <td className="px-6 py-4 whitespace-nowrap">
