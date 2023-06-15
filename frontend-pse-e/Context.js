a346bca8fd901639ad81f95c1de9569dc8e9d2ed
Saif Rashed <saif.rashed@hva.nl>
2023-06-15 13:01:37 +0200

    Manipulate assignment merged

--- frontend-pse-e/Context.js
+++ frontend-pse-e/Context.js
@@ -5,8 +5,9 @@ export const Context = createContext();
 export const Provider = ({ children }) => {
     const [course, setCourse] = useState(null);
     const [courses, setCourses] = useState([]);
+    const [assignment, setAssignment] = useState(null);
     return (
-        <Context.Provider value={{ course, setCourse, courses, setCourses }}>
+        <Context.Provider value={{ course, setCourse, courses, setCourses, assignment, setAssignment }}>
             {children}
         </Context.Provider>
     );
