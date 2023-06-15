b462c2904fc5be132c477883e5082f115116ee03
Saif Rashed <saif.rashed@hva.nl>
2023-06-15 11:58:40 +0200

    Buttons returned

--- frontend-pse-e/pages/courses/[courseId]/assignment/[assignmentId]/index.tsx
+++ frontend-pse-e/pages/courses/[courseId]/assignment/[assignmentId]/index.tsx
@@ -70,18 +70,23 @@ const Assignments = () => {
               <div className="col-span-1 p-4 ">
                 <div className="w-full p-4 bg-white rounded-lg border border-gray-200">
                   <div className="flex flex-col">
-                    <a
-                      className="inline-flex items-center gap-2  text-center rounded border border-indigo-600 px-8 py-3 text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
-                      onClick={() => { setShowPopup(!showPopup) }}
+
+                    <button onClick={() => { setShowPopup(!showPopup) }} className="bg-fuchsia-300 hover:bg-fuchsia-400 text-white w-full transition-all font-bold py-2 px-4 border-b-4 border-fuchsia-500 hover:border-fuchsia-500 rounded-lg flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                      </svg>
+                      <span className="ml-2">Submit</span>
+                    </button>
+
+                    <Link
+                      href={`/courses/${courseId}/assignment/${assignmentId}/submission/view`}
                     >
-                      <span className="text-sm font-medium"> Submit </span>
-                      <i className="fa-solid fa-upload"></i>
-                    </a>
-                    <Link href={`/courses/${courseId}/assignment/${assignmentId}/submission/view`}
-                      className="inline-flex items-center text-center gap-2 rounded border border-indigo-600 mt-2 px-8 py-3 bg-indigo-600 text-white hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
-                    >
-                      <span className="text-sm font-medium"> View </span>
-                      <i className="fa-solid fa-eye"></i>
+                      <button className="mt-5 bg-pink-300 hover:bg-pink-400 text-white w-full transition-all font-bold py-2 px-4 border-b-4 border-pink-500 hover:border-pink-500 rounded-lg flex max-width items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                        </svg>
+                        <span className="ml-2">View</span>
+                      </button>
                     </Link>
                   </div>
                 </div>
