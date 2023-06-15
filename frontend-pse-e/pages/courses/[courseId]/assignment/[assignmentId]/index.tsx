549c181943bfd790abdae64964e1f25a1f21bc78
Saif Rashed <saif.rashed@hva.nl>
2023-06-15 02:42:04 +0200

    Revert "extracted out in new page"
    This reverts commit b87db5896093cd8a066222a690e14f2beb74a647.

--- frontend-pse-e/pages/courses/[courseId]/assignment/[assignmentId]/index.tsx
+++ frontend-pse-e/pages/courses/[courseId]/assignment/[assignmentId]/index.tsx
@@ -113,96 +113,158 @@ const Assignments = () => {
           </div>
 
 
+          {!isTeacher ? (
 
-          <div className="grid grid-cols-1 gap-0 md:grid-cols-5 ">
-            <div className="col-span-1 p-4">
-              <div className="w-full p-4 bg-white rounded-lg shadow-lg">
-                <div key={assignment?.id}>
-                  <button
-                    className={`text-sm text-gray-500 underline cursor-pointer font-bold"}`}
-                  >
-                    Description
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="col-span-3 p-4">
-
-              <div className="w-full p-4 bg-white rounded-lg shadow-lg ">
-                <div className="flex space-x-4">
-                  <div key={assignment?.id} className="text-lg text-gray-800" dangerouslySetInnerHTML={{ __html: assignment?.description ? assignment?.description : "No description available" }}>
+            <div className="grid grid-cols-1 gap-0 md:grid-cols-5 ">
+              <div className="col-span-1 p-4">
+                <div className="w-full p-4 bg-white rounded-lg shadow-lg">
+                  <div key={assignment?.id}>
+                    <button
+                      className={`text-sm text-gray-500 underline cursor-pointer font-bold"}`}
+                    >
+                      Description
+                    </button>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col-span-1 p-4 ">
-              <div className="w-full p-4 bg-white rounded-lg shadow-lg">
-                <div className="flex flex-col">
+              <div className="col-span-3 p-4">
 
-                  <button onClick={togglePopup} className="w-full bg-fuchsia-300 hover:bg-fuchsia-400 text-white font-bold py-2 px-4 border-b-4 border-fuchsia-500 hover:border-fuchsia-500 rounded flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                    </svg>
-                    <span className="ml-2">Submit</span>
-                  </button>
-
-                  <UploadPopup showPopup={showPopup} togglePopup={togglePopup} />
-
-                  <Link
-                    href={`/courses/${courseId}/assignment/${assignmentId}/submission/view`}
-                  >
-                    <button className="w-full mt-5 bg-pink-300 hover:bg-pink-400 text-white font-bold py-2 px-4 border-b-4 border-pink-500 hover:border-pink-500 rounded flex max-width items-center">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                      </svg>
-
-                      <span className="ml-2">View</span>
-                    </button>
-                  </Link>
+                <div className="w-full p-4 bg-white rounded-lg shadow-lg ">
+                  <div className="flex space-x-4">
+                    <div key={assignment?.id} className="text-lg text-gray-800" dangerouslySetInnerHTML={{ __html: assignment?.description ? assignment?.description : "No description available" }}>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <p className="mt-8 text-gray-600">
-                {submission?.submitted_at ? (
-                  <span className="flex items-center text-green-500">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12"
-                      />
-                    </svg>
-                    <span className="ml-2">Submitted</span>
-                  </span>
-                ) : (
-                  <span className="flex items-center text-orange-500">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                    <span className="ml-2">Not Submitted</span>
-                  </span>
-                )}
+              <div className="col-span-1 p-4 ">
+                <div className="w-full p-4 bg-white rounded-lg shadow-lg">
+                  <div className="flex flex-col">
 
-                <span className="font-bold">Submission date: </span>{submission?.submitted_at ? formatDate(submission?.submitted_at) : "-"}</p>
+                    <button onClick={togglePopup} className="w-full bg-fuchsia-300 hover:bg-fuchsia-400 text-white font-bold py-2 px-4 border-b-4 border-fuchsia-500 hover:border-fuchsia-500 rounded flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                      </svg>
+                      <span className="ml-2">Submit</span>
+                    </button>
+
+
+                    <UploadPopup showPopup={showPopup} togglePopup={togglePopup} />
+
+
+                    <Link
+                      href={`/courses/${courseId}/assignment/${assignmentId}/submission/view`}
+                    >
+                      <button className="w-full mt-5 bg-pink-300 hover:bg-pink-400 text-white font-bold py-2 px-4 border-b-4 border-pink-500 hover:border-pink-500 rounded flex max-width items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                        </svg>
+
+                        <span className="ml-2">View</span>
+                      </button>
+                    </Link>
+                    {/* <Link
+                    href={`/courses/${courseId}/assignment/${assignmentId}/submission/grade`}
+                  >
+                    <button className="block w-full py-2 px-4 bg-yellow-500 text-white rounded-md mt-2">
+                      Grade submission
+                    </button>
+                  </Link> */}
+                  </div>
+                </div>
+                <p className="mt-8 text-gray-600">
+                  {submission?.submitted_at ? (
+                    <span className="flex items-center text-green-500">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12"
+                        />
+                      </svg>
+                      <span className="ml-2">Submitted</span>
+                    </span>
+                  ) : (
+                    <span className="flex items-center text-orange-500">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      <span className="ml-2">Not Submitted</span>
+                    </span>
+                  )}
+
+                  <span className="font-bold">Submission date: </span>{submission?.submitted_at ? formatDate(submission?.submitted_at) : "-"}</p>
+              </div>
             </div>
-          </div>
+          ) : null}
+
+          {isTeacher && submissions.length > 0 ? (
+            <div className="flex justify-center">
+              <div className="w-full relative overflow-x-auto shadow-md sm:p-2 md:p-4 lg:p-8 md:w-4/5">
+                <table className="w-full text-sm text-left bg-white">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-4 whitespace-nowrap">
+                        Name
+                      </th>
+                      <th scope="col" className="px-6 py-4 whitespace-nowrap">
+                        Student ID
+                      </th>
+                      <th scope="col" className="px-6 py-4 whitespace-nowrap">
+                        Submission Status
+                      </th>
+                      <th scope="col" className="px-6 py-4 whitespace-nowrap">
+                        Grade Status
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {submissions.map((submission) => (
+                      <tr
+                        key={submission.user_id}
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                      >
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <Link
+                            href={`/courses/${courseId}/assignment/${assignmentId}/submission/grade`}
+                          >
+                            {getUserDataById(submission.user_id)?.name || "Unknown"}
+                          </Link>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {submission.user_id}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {submission.submitted_at ? "Submitted" : "Not Submitted"}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {submission.grade ? "Graded" : "Not Graded"}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ) : null}
+
 
 
         </div>
