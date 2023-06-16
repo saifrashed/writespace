8008aec2b91005ef69563e3994b0d62357ef3bd2
Devran <devran_ilhan@live.nl>
2023-06-16 15:07:46 +0200

    fix again

--- frontend-pse-e/components/NavBar.tsx
+++ frontend-pse-e/components/NavBar.tsx
@@ -36,7 +36,7 @@ const NavBar = ({ showLogout = true }) => {
 
                             {showLogout && !isProfilePage && (
                             <Link href="/profile">
-                                <button type="button" className=" absolute -translate-x-10 inset-y-5 right-0 inline-flex p-2 ml-5 text-black transition-all duration-200 rounded-md focus:bg-gray-100 hover:bg-gray-100">
+                                <button type="button" className=" absolute -translate-x-10 inset-y-5 right-0 inline-flex p-2 ml-5 text-black transition-all duration-200 rounded-md focus:bg-gray-100 hover:bg-gray-100 items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
