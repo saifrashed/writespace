910e14aea488be523772dd3478c21a91520b7607
heckergorl <chezan.carmen@gmail.com>
2023-06-20 11:28:50 +0200

    quiz hook begonnen

--- frontend-pse-e/lib/hooks/useQuiz.ts
+++ frontend-pse-e/lib/hooks/useQuiz.ts
@@ -1,28 +1,63 @@
 import axios, { AxiosError } from "axios"
 import config from "../config";
 import { useState } from 'react';
+import { useNotification } from "./useNotification";
 
-function useQuiz() {
+function useQuiz(quizId: number) {
+  const { onSuccess, onError } = useNotification()
   const [gotQuizzes, setGotQuizzes] = useState(false);
   const [quizzes, setQuizzes] = useState([]);
 
-  const saveQuiz = async (userId: number, quizId: number, latestScore: number) => {
+
+  const getQuizzes = async () => {
     try {
-      const response = await axios.post(`${config.baseUrl}/quiz-score/save`, {
-        userId: userId,
-        quizId: quizId,
-        latestScore: latestScore
-      });
+      const response = await axios.get(`${config.baseUrl}/quiz/get-all`);
       return response.data;
     } catch (error) {
-      const axiosError = error as AxiosError;
-
-      if (axiosError && axiosError?.response?.status == 409) {
-        updateQuiz(userId, quizId, latestScore)
-      }
-      console.log(error)
+      console.log(error);
+      onError("Something went wrong");
     }
-  };
+  }
+
+  const getQuiz = async (quizId: number) => {
+    try {
+      const response = await axios.get(`${config.baseUrl}/quiz/get-quiz/${quizId}`,);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      onError("Something went wrong");
+    }
+  }
+
+  const saveQuiz = async (quizId: number, topic: string, ) => {
+    try {
+      const response = await axios.get(`${config.baseUrl}/quiz/save`, {quizId, topic, experiencePoints, });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      onError("Something went wrong");
+    }
+  }
+
+
+
+  // const saveQuiz = async (userId: number, quizId: number, latestScore: number) => {
+  //   try {
+  //     const response = await axios.post(`${config.baseUrl}/quiz-score/save`, {
+  //       userId: userId,
+  //       quizId: quizId,
+  //       latestScore: latestScore
+  //     });
+  //     return response.data;
+  //   } catch (error) {
+  //     const axiosError = error as AxiosError;
+
+  //     if (axiosError && axiosError?.response?.status == 409) {
+  //       updateQuiz(userId, quizId, latestScore)
+  //     }
+  //     console.log(error)
+  //   }
+  // };
 
   const updateQuiz = async (userId: number, quizId: number, latestScore: number) => {
     try {
@@ -37,16 +72,6 @@ function useQuiz() {
       console.log(error)
     }
   };
-
-  const getQuiz = async (userId: number) => {
-    try {
-      const response = await axios.get(`${config.baseUrl}/quiz-score/find-by-user-id/${userId}`);
-      setGotQuizzes(true);
-      setQuizzes(response.data)
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
 
   return { saveQuiz, getQuiz, gotQuizzes, quizzes };
