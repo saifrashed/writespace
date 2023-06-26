9d3e073f95daba309b330781b81fdcc830ef6d87
Devran <devran_ilhan@live.nl>
2023-06-26 20:40:57 +0200

    blur quiz

--- frontend-pse-e/components/quiz.jsx
+++ frontend-pse-e/components/quiz.jsx
@@ -55,7 +55,7 @@ const Quiz = () => {
             )}
 
             {isOpen && (
-                <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
+                <div className="backdrop-blur-[6px] fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
                     <div className="bg-white relative p-6 rounded-lg max-w-3xl overflow-y-auto max-h-[90vh] z-50">
                         <button type="button" onClick={closePopup} className="absolute top-3 right-2.5 text-black bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center " data-modal-hide="popup-modal">
                             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
