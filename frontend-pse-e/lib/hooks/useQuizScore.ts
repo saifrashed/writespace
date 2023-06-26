e4c1efa1d47d1a77ea2fa28714608b1dc1ed22d9
Saif Rashed <saif.rashed@hva.nl>
2023-06-26 13:40:19 +0200

    Fixed quiz

--- frontend-pse-e/lib/hooks/useQuizScore.ts
+++ frontend-pse-e/lib/hooks/useQuizScore.ts
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios";
 import config from "../config";
 import { useNotification } from "./useNotification";
 
@@ -39,7 +39,7 @@ function useQuizScore(token: string) {
     }
   }
 
-  // Retrieves the score for a specific quiz
+  // Retrieves a single score for a specific quiz
   const getOneScore = async (quizId: number, token: string) => {
     try {
       const response = await axios.post(`${config.baseUrl}/quiz-score/get-score`, { quizId }, { headers: { bearer: token } });
@@ -48,4 +48,31 @@ function useQuizScore(token: string) {
       console.log(error);
       onError("Something went wrong");
     }
+  }
 
+  // Saves a quiz score
+  const saveQuizScore = async (quizId: string, token: string, latestScore: number) => {
+    try {
+      const response = await axios.post(`${config.baseUrl}/quiz-score/save`, { quizId, latestScore }, { headers: { bearer: token } });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      onError("Something went wrong");
+    }
+  }
+
+  // Updates a quiz score
+  const updateQuizScore = async (quizId: number, token: string, latestScore: number) => {
+    try {
+      const response = await axios.post(`${config.baseUrl}/quiz-score/update/grade`, { quizId, latestScore }, { headers: { bearer: token } });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      onError("Something went wrong");
+    }
+  }
+
+  return { getAllQuizzesScores, getAllUserScores, getAllQuizScores, getOneScore, saveQuizScore, updateQuizScore };
+}
+
+export default useQuizScore;
