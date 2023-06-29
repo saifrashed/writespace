d9827c3050c2eb8cf586ac241a6cbe8912dbd38e
Saif Rashed <saif.rashed@hva.nl>
2023-06-29 17:24:42 +0200

    Fixed replies

--- frontend-pse-e/pages/courses/[courseId]/assignment/[assignmentId]/submission/grade.tsx
+++ frontend-pse-e/pages/courses/[courseId]/assignment/[assignmentId]/submission/grade.tsx
@@ -442,10 +442,13 @@ const Grade: React.FC = () => {
                                                                                 className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-primary-800"
                                                                                 onClick={(e) => {
                                                                                     e.preventDefault();
-                                                                                    const comment = document.getElementById(`${note.id}-comment`).value;
-                                                                                    if (comment) {
-                                                                                        addCommentReply(note.id, comment);
-                                                                                        document.getElementById(`${note.id}-comment`).value = "";
+                                                                                    const commentElement = document.getElementById(`${note.id}-comment`) as HTMLInputElement;
+                                                                                    if (commentElement !== null) {
+                                                                                        const comment = commentElement.value;
+                                                                                        if (comment) {
+                                                                                            addCommentReply(note.id, comment);
+                                                                                            commentElement.value = "";
+                                                                                        }
                                                                                     }
                                                                                 }}
                                                                             >
