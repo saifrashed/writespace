ed945db65448fd0ed71ffd305e34423e4ae9a257
Saif Rashed <saif.rashed@hva.nl>
2023-06-30 07:52:18 +0200

    test 2

--- frontend-pse-e/lib/pdfToText.js
+++ frontend-pse-e/lib/pdfToText.js
@@ -2,23 +2,23 @@ import { getDocument } from 'pdfjs-dist/legacy/build/pdf';
 import { pdfjs } from "react-pdf";
 
 // Specify the correct path to the worker script
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
-pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+// import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+// pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 
 const convertPdfToText = async (fileUrl) => {
   try {
-    const loadingTask = getDocument(fileUrl);
-    const pdf = await loadingTask.promise;
-    const totalNumPages = pdf.numPages;
+    // const loadingTask = getDocument(fileUrl);
+    // const pdf = await loadingTask.promise;
+    // const totalNumPages = pdf.numPages;
     let text = "";
 
-    for (let i = 1; i <= totalNumPages; i++) {
-      const page = await pdf.getPage(i);
-      const content = await page.getTextContent();
-      const pageText = content.items.map((item) => item.str).join(" ");
-      text += pageText + " ";
-    }
+    // for (let i = 1; i <= totalNumPages; i++) {
+    //   const page = await pdf.getPage(i);
+    //   const content = await page.getTextContent();
+    //   const pageText = content.items.map((item) => item.str).join(" ");
+    //   text += pageText + " ";
+    // }
 
     return text;
   } catch (error) {
